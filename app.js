a8f8247d0639903030b6813612812afd5ce8fc08
timaroblox5 <timaroblox9@gmail.com>
2024-08-19 00:47:28 +0300

    z

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
 
 const client = new Client({ 
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-const TOKEN = 'YOUR_BOT_TOKEN';
+const TOKEN = process.env.DISCORD_TOKEN;
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
